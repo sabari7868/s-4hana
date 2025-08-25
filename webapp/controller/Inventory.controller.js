05553d4250775933953d2bb18e70829edb3d8fef
sabari7868 <sabarisurya540@gmail.com>
2025-08-25 19:16:43 +0530

    qwert

--- webapp/controller/Inventory.controller.js
+++ webapp/controller/Inventory.controller.js
@@ -1,20 +1,10 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/ui/model/Filter",
-  "sap/ui/model/FilterOperator",
-  "sap/m/MessageToast"
-], function (Controller, Filter, FilterOperator, MessageToast) {
+  "sap/ui/core/mvc/Controller"
+], function(Controller) {
   "use strict";
-
-  return Controller.extend("inventory.app.controller.Inventory", {
-
-    onInit: function () {
-      var oModel = this.getView().getModel();
-      oModel.read("/A_MaterialStock", {
-        success: function () { console.log("OData loaded"); },
-        error: function (oError) { console.error(oError); }
-      });
+  return Controller.extend("inventoryapp.controller.Inventory", {
+    onInit: function() {
+      // Model automatically loaded from manifest.json
     }
-
   });
 });
