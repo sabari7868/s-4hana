db4477c9c256eca7339e2670c78458244324f414
sabari7868 <sabarisurya540@gmail.com>
2025-08-25 15:35:20 +0530

    new-commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,19 +1,3 @@
-// sap.ui.define([
-//   "sap/ui/core/UIComponent",
-//   "sap/ui/model/json/JSONModel"
-// ], function (UIComponent, JSONModel) {
-//   "use strict";
-
-//   return UIComponent.extend("inventory.app.Component", {
-//     metadata: {
-//       manifest: "json"
-//     },
-
-//     init: function () {
-//       UIComponent.prototype.init.apply(this, arguments);
-//     }
-//   });
-// });
 sap.ui.define([
   "sap/ui/core/UIComponent"
 ], function (UIComponent) {
@@ -25,9 +9,8 @@ sap.ui.define([
     },
 
     init: function () {
-      // call the base component's init function
+      // Call base component init
       UIComponent.prototype.init.apply(this, arguments);
-      // nothing else is needed here unless you want to customize routing or other behavior
     }
   });
 });
